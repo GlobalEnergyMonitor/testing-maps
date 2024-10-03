0ff7378fcab55c8c496c57866a8689403b930a52
Taylor Higgins <126080744+TaylorHigginsGEM@users.noreply.github.com>
2024-10-03 13:51:18 -0500

    Update config.js style instructions

--- trackers/africa-energy-all-test/config.js
+++ trackers/africa-energy-all-test/config.js
@@ -177,6 +177,9 @@ var config = {
         'areas-subnat-sat-display': {'display': 'location'}, 
         // 'areas-display': {'display': 'location'} // TODO pull out first one only if ; in it
     },
+
+/* style test parameters!
+
 /* Mapbox styling applied to all trackers */
 pointPaint: {
     'circle-opacity':.75
